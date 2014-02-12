47d128eda53f39aa225764adb83b920801094310
Randy Pray <rpray@netins.net>
2014-02-12 01:22:28 -0600

    changed start_system to start_extra_system

--- lib/te3270/emulators/passport.rb
+++ lib/te3270/emulators/passport.rb
@@ -5,7 +5,7 @@ module TE3270
   module Emulators
     class Passport < Extra
       private
-      def start_system
+      def start_extra_system
         begin
           @system = WIN32OLE.new('PASSPORT.System')
         rescue Exception => e
